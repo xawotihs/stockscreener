7097ee6b93c3266d82b63cd901e5163410bc443c
xawotihs <xawotihs@gmail.com>
2024-08-11 18:37:40 +0200

    More data and analysis

--- main.py
+++ main.py
@@ -2,24 +2,41 @@ import yfinance as yf
 import pandas as pd
 import streamlit as st
 import sys
-from datetime import date
+from datetime import date,datetime, timedelta
 
 # Function to fetch stock data
 def fetch_stock_data(ticker):
+    print(f'Fetching data for {ticker}')
     stock = yf.Ticker(ticker)
     info = stock.info
+
+    fiveYearAvgDividendYield = stock.info.get('fiveYearAvgDividendYield')
+    dividendRate = stock.info.get('dividendRate')
+    close = stock.info.get('previousClose')
+    if fiveYearAvgDividendYield is None or dividendRate is None or fiveYearAvgDividendYield == 'N/A':
+        fiveYearAvgDividendYield = 0
+        discount = 0
+    else:
+        if stock.info.get('currency') != 'GBp':
+            discount = 100*(close-dividendRate/fiveYearAvgDividendYield*100)/(close)
+        else:
+            discount = 100*(close-(dividendRate*100)/fiveYearAvgDividendYield*100)/(close)
+
     try:
         return {
             'ticker': ticker,
             'name': info.get('shortName', ''),
             'sector': info.get('sector', ''),
+            'dividend_streak': calculate_dividend_streak(stock),
             'dividend_yield': round(info.get('dividendYield', 0) * 100, 2),
+            '5y_Agv_dividend_yield': round(fiveYearAvgDividendYield, 2),
             'payout_ratio': round(info.get('payoutRatio', 0), 2),
             'dividend_growth_rate': round(calculate_dividend_growth_rate(stock), 2),
             'eps': round(info.get('trailingEps', 0), 2),
             'pe_ratio': round(info.get('trailingPE', 0), 2),
             'debt_to_equity': round(info.get('debtToEquity', 0), 2),
-            'roe': round(info.get('returnOnEquity', 0), 2)
+            'roe': round(info.get('returnOnEquity', 0), 2),
+            'discount/premium':round(discount,2)
         }
     except KeyError:
         return None
@@ -28,7 +45,7 @@ def fetch_stock_data(ticker):
 def calculate_dividend_growth_rate(stock):
     dividends = stock.dividends
 
-    resampled = dividends.resample('Y').sum()
+    resampled = dividends.resample('YE').sum()
     if len(resampled) < 1:
         return 0
     
@@ -43,6 +60,65 @@ def calculate_dividend_growth_rate(stock):
     
     return ((resampled.iloc[idx] / resampled.iloc[idx-5]) ** (1 / 5) - 1) * 100
 
+# Function to filter bad dividends data in Yahoo Finance
+def filter_wrong_dividends(df):
+  prevValue = -1
+  prevIndex = -1
+
+  filteredDf = pd.DataFrame(columns = df.columns,
+                   index = df.index)
+
+  for index, row in df.iterrows():
+    if (prevValue == -1) :
+          prevValue = row['Dividends']
+          prevIndex = index
+          filteredDf.loc[index] = row
+    else:
+        #filtering dividend data if they are duplicated in less then 2 days
+        if (prevValue == row['Dividends']) and (index - prevIndex < timedelta(days=2)):
+            print(index)
+            print(prevIndex)
+            print(prevValue)
+            print(row['Dividends'])
+        else:
+          filteredDf.loc[index] = row
+
+        prevIndex = index
+        prevValue = row['Dividends']
+
+  filteredDf = filteredDf.dropna()
+
+  return filteredDf
+
+# Function to calculate dividend streak
+def calculate_dividend_streak(stock):
+    dividends_df = stock.dividends.to_frame()
+    filtered_dividends_df = filter_wrong_dividends(dividends_df)
+    dividends = dividends_df.squeeze()
+
+    if type(dividends) != pd.Series :
+        return 0
+
+    resampled = dividends.resample('YE').sum()
+    if len(resampled) < 1:
+        return 0
+    
+    prev = 0
+    prevprev = 0
+    ctr = 0
+    for i in range(len(resampled)-1):
+        if resampled.iloc[i] >= prev:
+            ctr += 1
+        else:
+            # try to support the case where there is exceptional dividends in the middle of a streak
+            if resampled.iloc[i] >= prevprev and prevprev > 0:
+                ctr += 1
+            else:
+                ctr = 0
+        prevprev = prev
+        prev = resampled.iloc[i]
+
+    return ctr
 
 # Read stock tickers from file
 def read_stock_tickers(file_path):
@@ -64,9 +140,10 @@ def calculate_metrics(row):
         row['eps'] > 0,
         row['pe_ratio'] < 20,
         row['debt_to_equity'] < 100,
-        row['roe'] > 0.10
+        row['roe'] > 0.10,
+        row['discount/premium'] < 0
     ])
-    negatives = 7 - positives
+    negatives = 8 - positives
     return positives, negatives
 
 stock_df[['positives', 'negatives']] = stock_df.apply(lambda row: calculate_metrics(row), axis=1, result_type="expand")
@@ -99,6 +176,8 @@ def highlight_metrics(row):
             colors.append('background-color: green' if row[col] < 100 else 'background-color: red')
         elif col == 'roe':
             colors.append('background-color: green' if row[col] > 0.10 else 'background-color: red')
+        elif col == 'discount/premium':
+            colors.append('background-color: green' if row[col] < 0 else 'background-color: red')
         else:
             colors.append('')
     return colors
